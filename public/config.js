3f7fb2762b9ec5a410a340a1fb78ffa70020632b
Jesse Engerman <jesseengerman@hotmail.com>
2020-01-26 21:17:43 -0600

    config setup

---
+++ public/config.js
@@ -0,0 +1,26 @@
+var config = {
+    header:{
+        name: "jesse engerman",
+        intro: "software, games and web"
+    },
+
+    main: {
+        projects: [
+            {
+                title: "polywolf",
+                description: ""
+            },
+            {
+
+            }
+        ],
+
+        resume: {
+
+        },
+
+        contact: {
+
+        }
+    }
+}
